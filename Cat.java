7e6d8d48cbb83f82a66eae6f5ebb5eb5603c6e8c
Anamitr <sochacki.konrad@gmail.com>
2016-06-22 13:00:24 +0200

    1.01

--- Cat.java
+++ Cat.java
@@ -1,19 +1,32 @@
+import java.util.Date;
+
 public class Cat {
 	String name, owner;
+	Date birthday;
+	Float weight;	
 
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
 	public String getOwner() {
 		return owner;
 	}
-
 	public void setOwner(String owner) {
 		this.owner = owner;
+	}
+	public Date getBirthday() {
+		return birthday;
+	}
+	public void setBirthday(Date birthDay) {
+		this.birthday = birthDay;
+	}
+	public Float getWeight() {
+		return weight;
+	}
+	public void setWeight(Float weight) {
+		this.weight = weight;
 	}
 }
--- Interface.java
+++ Interface.java
@@ -1,4 +1,7 @@
+import java.util.Date;
 import java.util.Scanner;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class Interface {
     static Scanner input = new Scanner(System.in);
@@ -9,8 +12,27 @@ public class Interface {
         System.out.print("Podaj imiê kota: ");
         kot.setName(input.nextLine());
 
-        System.out.print("Podaj, kto jest opiekunem kota: ");
+        System.out.print("Podaj kto jest opiekunem kota: ");
         kot.setOwner(input.nextLine());
+        
+        System.out.print("Podaj date urodzin kota: ");
+        SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");            
+        try {
+			kot.setBirthday(sdf.parse(input.nextLine()));
+		} catch (ParseException e) {
+			System.out.println("Error! ParseException");
+			System.exit(0);
+		}       
+        
+        System.out.print("Podaj wage kota: ");
+        try {
+        	kot.setWeight(Float.parseFloat(input.nextLine()));
+        } catch(NumberFormatException e) {
+        	System.out.println("Error! NumberFormatException");
+			System.exit(0);
+        }        
+        
+        //System.out.println(kot.getBirthday() + "   " + kot.getWeight());
 
         System.out.println("Dziêkujê, teraz ju¿ wiem prawie wszystko o kocie!");
     }
