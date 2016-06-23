10cd135d776a24fcf2193c5f0c9524dbff832e6c
Anamitr <sochacki.konrad@gmail.com>
2016-06-23 07:46:03 +0200

    fix

--- Interface.java
+++ Interface.java
@@ -1,7 +1,5 @@
 import java.util.Date;
 import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
@@ -11,23 +9,28 @@ public class Interface {
     public static void main(String[] args) {
         Cat kot = new Cat();
 
-/*        System.out.print("Podaj imiê kota: ");
+        System.out.print("Podaj imiê kota: ");
         kot.setName(input.nextLine());
 
         System.out.print("Podaj kto jest opiekunem kota: ");
-        kot.setOwner(input.nextLine());*/
+        kot.setOwner(input.nextLine());
         
         System.out.print("Podaj date urodzin kota: ");
-        Pattern date = Pattern.compile("([0-9]{2}\\.){2}[0-2][0-9]{3}");//Pattern.compile("([1-9]{2}\\.){2}[0-2][1-9]{3}");
-        Matcher macher = date.matcher(input.nextLine());  
-        System.out.println(macher.matches());
+        SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");            
+        try {
+			kot.setBirthday(sdf.parse(input.nextLine()));
+		} catch (ParseException e) {
+			System.out.println("Error! ParseException");
+			System.exit(0);
+		}       
+        
         System.out.print("Podaj wage kota: ");
         try {
         	kot.setWeight(Float.parseFloat(input.nextLine()));
         } catch(NumberFormatException e) {
         	System.out.println("Error! NumberFormatException");
 			System.exit(0);
-        }
+        }        
         
         //System.out.println(kot.getBirthday() + "   " + kot.getWeight());
 
